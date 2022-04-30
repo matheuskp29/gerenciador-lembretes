9547d6fe360d0a53756f772a983f3622bd76c5b6
Matheus Kaczan <matheuskp29@gmail.com>
2022-04-29 21:29:11 -0300

    Lista simples com text

--- App.js
+++ App.js
@@ -3,10 +3,21 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [lembrete, setLembrete] = useState('');
+  const [lembretes, setLembretes] = useState([]);
 
   const capturarLembrete = (lembreteDigitado) => {
     setLembrete(lembreteDigitado)
   }
+
+  const adicionarLembrete = () => {
+    setLembretes(lembretes => {
+      let aux = [lembrete, ...lembretes]
+      setLembrete('')
+      return aux
+    })
+
+  }
+
   return (
     <View
     style={styles.telaPrincipalView}>
@@ -16,16 +27,21 @@ export default function App() {
           placeholder='Lembrar...'
           style={styles.lembreteTextInput}
           onChangeText={capturarLembrete}
+          value={lembrete}
         />
         <Button
           disabled={lembrete.length === 0}
           title='Adicionar lembrete'
+          onPress={adicionarLembrete}
         />
         <Text>{lembrete}</Text>
       </View>
       <View>
         {/* aqui será exibida a lista de lembretes */}
-
+        {/* Fazer café => <Text>Fazer café</Text> */}
+        {
+          lembretes.map(l => <Text>{l}</Text>)
+        }
       </View>
     </View>
   );
